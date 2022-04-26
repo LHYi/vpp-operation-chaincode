33409a6141e6bad63efe3398d97a8861fc55b240
LHYi <402927348@qq.com>
2022-04-26 10:31:07 +0800

    test update all func

--- vpp-operation-chaincode.go
+++ vpp-operation-chaincode.go
@@ -15,66 +15,65 @@ type SmartContract struct {
 
 // Asset describes basic details of what makes up a simple asset
 type Asset struct {
-	ID                   string  `json:"ID"`
-	Regulation_direction string  `json:"Regulation_direction"`
-	CurrentTime          string  `json:"CurrentTime"`
-	NextBidTime          string  `json:"NextBidTime"`
-	P_available          float64 `json:"P_available"`
-	Price_bid            float64 `json:"Price_bid"`
-	Price_cleared        float64 `json:"Price_cleared"`
-	P_cap                float64 `json:"P_cap"`
-	Regulation_signal    float64 `json:"Regulation_signal"`
-	P_reg                float64 `json:"P_reg"`
-	P_res                float64 `json:"P_res"`
-	P_mis_1              float64 `json:"P_mis_1"`
-	P_mis_2              float64 `json:"P_mis_2"`
-	Uncertainty          float64 `json:"Uncertainty"`
-	Price_penalty        float64 `json:"Price_penalty"`
-	Revenue_capacity     float64 `json:"Revenue_capacity"`
-	Revenue_mileage      float64 `json:"Revenue_mileage"`
-	Penalty              float64 `json:"Penalty"`
+	ID                string  `json:"ID"`
+	CurrentTime       string  `json:"CurrentTime"`
+	NextBidTime       string  `json:"NextBidTime"`
+	P_available       float64 `json:"P_available"`
+	Price_bid         float64 `json:"Price_bid"`
+	Price_cleared     float64 `json:"Price_cleared"`
+	P_cap             float64 `json:"P_cap"`
+	Regulation_signal float64 `json:"Regulation_signal"`
+	P_reg             float64 `json:"P_reg"`
+	P_res             float64 `json:"P_res"`
+	P_mis_1           float64 `json:"P_mis_1"`
+	P_mis_2           float64 `json:"P_mis_2"`
+	Uncertainty       float64 `json:"Uncertainty"`
+	Price_penalty     float64 `json:"Price_penalty"`
+	Revenue_capacity  float64 `json:"Revenue_capacity"`
+	Revenue_mileage   float64 `json:"Revenue_mileage"`
+	Penalty           float64 `json:"Penalty"`
 }
 
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	assets := []Asset{
-		{ID: "DER-1", Regulation_direction: "Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-1-Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-1", Regulation_direction: "Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-1-Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-2", Regulation_direction: "Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-2-Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-2", Regulation_direction: "Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-2-Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-3", Regulation_direction: "Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-3-Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-3", Regulation_direction: "Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-3-Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-4", Regulation_direction: "Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-4-Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-4", Regulation_direction: "Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-4-Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-5", Regulation_direction: "Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-5-Up", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
-		{ID: "DER-5", Regulation_direction: "Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
+		{ID: "DER-5-Down", CurrentTime: "04-22-01", NextBidTime: "04-22-03",
 			P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
 			P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
 			Revenue_mileage: 0, Penalty: 0},
@@ -86,19 +85,6 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 			return err
 		}
 
-		eventFrom, err0 := ctx.GetClientIdentity().GetMSPID()
-		if err0 != nil {
-			return fmt.Errorf("Failed to get client identity")
-		}
-
-		payload := "Ledger Initialized by " + eventFrom
-		payloadAsBytes := []byte(payload)
-
-		eventErr := ctx.GetStub().SetEvent(eventFrom, payloadAsBytes)
-		if eventErr != nil {
-			return fmt.Errorf("%v", eventErr)
-		}
-
 		err = ctx.GetStub().PutState(asset.ID, assetJSON)
 		if err != nil {
 			return fmt.Errorf("failed to put to world state. %v", err)
@@ -106,6 +92,151 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	}
 
 	return nil
+}
+
+// AddDER adds a new DER to the ledger
+func (s *SmartContract) AddDER(ctx contractapi.TransactionContextInterface, id string, CurrentTime string, NextBidTime string) error {
+	exists, err := s.DERExists(ctx, id)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the DER %s already exists", id)
+	}
+
+	asset := Asset{
+		ID: id, CurrentTime: CurrentTime, NextBidTime: NextBidTime,
+		P_available: 0, Price_bid: 0, Price_cleared: 0, P_cap: 0, Regulation_signal: 0, P_reg: 0,
+		P_res: 0, P_mis_1: 0, P_mis_2: 0, Uncertainty: 0, Price_penalty: 0, Revenue_capacity: 0,
+		Revenue_mileage: 0, Penalty: 0,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// ReadDER returns the asset stored in the world state with given id.
+func (s *SmartContract) ReadDER(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the DER %s does not exist", id)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &asset, nil
+}
+
+// GetAllDERs returns all DERs found in world state
+func (s *SmartContract) GetAllDERs(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var assets []*Asset
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
+	}
+
+	return assets, nil
+}
+
+// UpdateTime updates an existing asset in the world state with provided parameters.
+func (s *SmartContract) UpdateTime(ctx contractapi.TransactionContextInterface, id string, currentTime string, nextBidTime string) error {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return fmt.Errorf("the DER %s does not exist", id)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return err
+	}
+
+	asset.CurrentTime = currentTime
+	asset.NextBidTime = nextBidTime
+
+	assetJSON, err = json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// UpdateTime updates an existing asset in the world state with provided parameters.
+func (s *SmartContract) UpdateAllTime(ctx contractapi.TransactionContextInterface, id string, currentTime string, nextBidTime string) error {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return err
+	}
+	defer resultsIterator.Close()
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return err
+		}
+		id = asset.ID
+		asset.CurrentTime = currentTime
+		asset.NextBidTime = nextBidTime
+		assetJSON, err1 := json.Marshal(asset)
+		if err1 != nil {
+			return err1
+		}
+		err = ctx.GetStub().PutState(id, assetJSON)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// DERExists returns true when DER with given ID exists in world state
+func (s *SmartContract) DERExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+	assetJSON, err := ctx.GetStub().GetState(id)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return assetJSON != nil, nil
 }
 
 func main() {
